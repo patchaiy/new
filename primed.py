6203cdb1e241674ff96af8ba2550f542135dea45
patchaiy <50619625+patchaiy@users.noreply.github.com>
2019-06-25 12:38:44 +0530

    Create primed.py

---
+++ primed.py
@@ -0,0 +1,19 @@
+num = int(input())
+
+# take input from the user
+# num = int(input("Enter a number: "))
+
+# prime numbers are greater than 1
+if num > 1:
+   # check for factors
+   for i in range(2,num):
+       if (num % i) == 0:
+           print("no")
+           break
+   else:
+       print("yes")
+       
+# if input number is less than
+# or equal to 1, it is not prime
+else:
+   print("no")
