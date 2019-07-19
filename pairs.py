2eb3e7786d78e6f2cf8bd1a01c6c9b9875f8ad9b
patchaiy <50619625+patchaiy@users.noreply.github.com>
2019-07-19 11:17:22 +0530

    Create pairs.py

---
+++ pairs.py
@@ -0,0 +1,16 @@
+numi=input()
+a=list(map(int,numi.split()))
+k=a[1]
+h=input()
+flag=0
+sv=list(map(int,h.split()))
+for i in range(0,len(sv)-1):
+	for j in range(i+1,len(sv)):
+		if sv[i]+sv[j]==k:
+			print("yes")
+			flag=1
+			break
+	if flag==1:
+		break
+if flag!=1:
+	print("no")
